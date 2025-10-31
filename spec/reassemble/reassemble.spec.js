ba298b4155663ad47f087d5ec2cd4021e6ef7863
Pavlo Voropaiev <pavel.voropaiev@elastic.io>
2025-10-31 17:25:56 +0200

    feat: Fix race condition in Re-assemble action, update dependencies, and enhance tests\n\n- Fixes a sporadic race condition in the "Re-assemble Messages" action that could cause a  error when a group was emitted. (See #105)\n- Added unit tests to cover the race condition and prevent future regressions.\n- Added a unit test to verify the  overload protection.\n- Updated major dependencies:  to  and  to  (due to these major bumps, the component version is now 1.6.0).\n- Updated  to  and Node.js engine to  in  and .\n- Refactored  import to use a local utility instead of  in  and .\n- Updated CHANGELOG.md and component.json version to 1.6.0.

--- spec/reassemble/reassemble.spec.js
+++ spec/reassemble/reassemble.spec.js
@@ -5,6 +5,7 @@ const sinon = require('sinon');
 const chaiAsPromised = require('chai-as-promised');
 const { sleep } = require('@elastic.io/component-commons-library');
 const reassemble = require('../../lib/actions/reassemble');
+const { GroupsProcessor } = require('../../lib/utils');
 const { getContext } = require('../common');
 const testData = require('./testData.json');
 
@@ -104,5 +105,71 @@ describe('Reassemble', () => {
     expect(context.emit.callCount).to.be.equal(1);
     const { messageData } = context.emit.getCall(0).lastArg.body;
     expect(Object.values(messageData)[0]).to.deep.equal(testData);
+  });
+
+  it('should emit immediately if groupDataSize exceeds MAX_LOCAL_STORAGE_SIZE', async () => {
+    const smallData = { content: 'a' };
+    const msg = {
+      body: {
+        groupId: 'largeGroup',
+        groupSize: 10,
+        timersec: 5000,
+        messageData: smallData,
+      },
+    };
+    const cfg = { mode: 'groupSize&timeout', emitAsArray: true };
+    const context = getContext();
+
+    const realStringify = JSON.stringify;
+    const stringifyStub = sinon.stub(JSON, 'stringify');
+    stringifyStub.callsFake((val) => {
+      if (val === smallData) {
+        return 'a'.repeat(1024 * 1024 * 6); // 6MB
+      }
+      return realStringify(val);
+    });
+
+    await reassemble.process.call(context, msg, cfg);
+
+    expect(context.emit.callCount).to.be.equal(1);
+    expect(context.emit.getCall(0).lastArg.body.messageData[0]).to.deep.equal(smallData);
+
+    stringifyStub.restore();
+  });
+});
+
+describe('GroupsProcessor', () => {
+  let context;
+
+  beforeEach(() => {
+    context = getContext();
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should not throw an error if checkAndEmitGroup is called concurrently', async () => {
+    const cfg = { mode: 'groupSize&timeout', emitAsArray: true };
+    const processor = new GroupsProcessor(context, cfg);
+
+    const deleteStub = sinon.stub(processor.maester, 'deleteObjectById').resolves();
+
+    const groupId = 'group123';
+    const groupUniqueId = `flow123/step123/${groupId}`;
+    processor.groupsStorage[groupUniqueId] = {
+      messages: [{ msg1: {} }, { msg2: {} }],
+      groupSize: 2,
+      maesterId: 'maester-123',
+      readyAfter: Date.now() + 10000,
+    };
+
+    // Simulate concurrent calls
+    const promise1 = processor.checkAndEmitGroup(groupUniqueId);
+    const promise2 = processor.checkAndEmitGroup(groupUniqueId);
+
+    await Promise.all([promise1, promise2]);
+
+    expect(deleteStub.callCount).to.be.equal(1);
   });
 });
